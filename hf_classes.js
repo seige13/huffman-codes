e3feb1bebb56a0ee04cbab94e05866a7894d82d6
vsmagin <vsmagin@gmail.com>
2018-03-29 10:52:55 -0400

    generating haffman codes

---
+++ hf_classes.js
@@ -0,0 +1,74 @@
+
+class TreeNode {
+    constructor(left, right, parent) {
+        this.leftchild = left;      //index of left child in array
+        this.rightchild = right;    //index of right child in array
+        this.parent = parent;       //index of parent in array
+    }
+};
+
+class SymbolInfo {
+    constructor(symbol, frequency, leaf) {
+        this.symbol = symbol;       //character from text
+        this.frequency = frequency; //frequency of the character
+        this.leaf = leaf;           //TreeNode for this character
+    }
+}
+
+class ForestRoots {
+    constructor(weight, root) {
+        this.weight = weight;   //combined frequency of all the chanracters belonging to this root
+        this.root = root;       //index of this root in array
+    }
+}
+
+class PriorityQueue {
+    constructor(prioritizer) {
+        this.arr = [];
+        this.prioritizer = prioritizer;
+    }
+    enqueu(forestRoot) {
+        if (this.arr.length == 0) {
+            this.arr.push(forestRoot);
+            return;
+        }
+        let added = false;
+        for (let i = 0; i < this.arr.length; i++) {
+            if (this.prioritizer(forestRoot.weight, this.arr[i].weight) < 0) {
+                continue;
+            }
+            this.arr.splice(i, 0, forestRoot);
+            added = true;
+            break;
+            // else if (this.prioritizer(forestRoot.weight, this.arr[i].weight) == 0) {
+            //     this.arr.splice(i+1, 0, forestRoot);
+            //     added = true;
+            //     break;
+            // }
+            // else {
+            //     this.arr.splice(i, 0, forestRoot);
+            //     added = true;
+            //     break;
+            // }
+        }
+        if (!added) {
+            this.arr.push(forestRoot);
+        }
+    }
+    dequeue() {
+        return this.arr.shift();
+    }
+    peek() {
+        return this.arr[0];
+    }
+    get length() {
+        return this.arr.length;
+    }
+};
+
+module.exports = {
+    PriorityQueue: PriorityQueue,
+    TreeNode: TreeNode,
+    SymbolInfo: SymbolInfo,
+    ForestRoots: ForestRoots,
+  }
--- index.js
+++ index.js
@@ -1,13 +1,27 @@
 const fs = require('fs');
+const path = require("path");
+const readline = require("readline");
+const classes = require('./hf_classes');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 
 // Read and process test file
-fs.readFile('test.txt', 'utf8', function (error, data) {
-    if (error) {
-        throw error;
-    }
-
-    processString(data);
+rl.question('Please enter the  input file path: ', (answer) => {
+    let filePath = answer || path.join(__dirname, 'infile.dat');
+    fs.readFile(filePath, 'utf8', function (error, data) {
+        if (error) {
+            throw error;
+        }
+    
+        console.log(data);
+        processString(data);
+    });
+    rl.close();
 });
+
 
 /**
  * Counts the char frequency of a string
@@ -50,6 +64,8 @@ function processString(data) {
         );
 
     printFrequency(charCodeArray);
+    generateHCodes(charCodeArray);
+    
 }
 
 /**
@@ -62,5 +78,75 @@ function printFrequency(data) {
     for (charCode of data) {
         console.log(`  ${charCode.char},    ${(charCode.frequency * 100).toFixed(2)}% `);
     }
+}
+
+/**
+ * Generate Huffman Codes for characters
+ * 
+ * @param {Array} data 
+ */
+function generateHCodes(data) {
+    let tree = [];      //sequential container (list/array/vector) named tree of the type TreeNode
+    let alphabet = [];      //sequential container named alphabet of type SymbolInfo
+    let forest = new classes.PriorityQueue((x, y) => y - x);    //priority queue of data structures that represent the trees themselves
+
+    // generate tree, alphabet, and forest arrays from char data
+    for (let i = 0; i < data.length; i++) {
+
+        tree[i] = new classes.TreeNode();           //tree
+
+        let symbolInfo = new classes.SymbolInfo();  //alphabet
+        symbolInfo.symbol = data[i].char;
+        symbolInfo.frequency = data[i].count;
+        symbolInfo.leaf = i;
+        alphabet[i] = symbolInfo;
+
+        let forestRoot = new classes.ForestRoots(); //forest
+        forestRoot.weight = data[i].count;
+        forestRoot.root = i;
+        forest.enqueu(forestRoot);
+
+    }
+
+    // fill up forest priority queue
+    let i = tree.length;
+    while (forest.length > 1) {
+        let least = forest.dequeue();
+        let second = forest.dequeue();
+
+        let newNode = new classes.TreeNode();
+        newNode.leftchild = least.root;
+        newNode.rightchild = second.root;
+
+        tree[least.root].parent = i;
+        tree[second.root].parent = i;
+        tree[i] = newNode;
+
+        let newRoot = new classes.ForestRoots();
+        newRoot.weight = least.weight + second.weight;
+        newRoot.root = i++;
+        forest.enqueu(newRoot);
+    }
+
+    // generate Huffman Codes
+    var codes = {};
+    for (let i = 0; i < alphabet.length; i++) {
+        let code = [];
+        let j = i;
+        while (j != tree.length-1) {
+            let currentIndex = j;
+            if (tree[j].parent)     // if there is a parent
+                j = tree[j].parent; // move up the tree
+            if (tree[j].leftchild == currentIndex) {    //if this is a left left child
+                code.unshift(0);                        //add 0 to code
+            }
+            else {                                      //if this is a right child
+                code.unshift(1);                        //add 1 to code
+            }
+        }
+        codes[alphabet[i].symbol] = code.join('');
+    }
+
+    console.log(codes);
 }
 
